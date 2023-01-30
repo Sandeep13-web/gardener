04620fec8e91fa076dbfbcf25c3c486f22df42b6
ek_mac_46 <ek_mac_46@Ganesh339.local>
2023-01-30 14:33:39 +0545

    feat: added xframe options in nextconfig to prevent clickjacking

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)?', // Matches all pages
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
