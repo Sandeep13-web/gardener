c1a792f1fe395c5cf35eb73a9bfa9989928c53f1
Ganesh Raj Manandhar <ek_mac_46@Ganesh339.local>
2023-03-21 11:02:01 +0545

    fix: fixing unit testing by mocking next router and also installing next-router-mock

--- tests/pages/auth/login/index.test.js
+++ tests/pages/auth/login/index.test.js
@@ -1,8 +1,21 @@
 import Login from '@pages/auth/login/index';
-import { render } from '@testing-library/react';
+import { store } from '@store/index';
+import { Provider } from 'react-redux';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      pathname: '/',
+      // ... whatever else you you call on `router`
+    };
+  },
+}));
+
 
 describe('Login', () => {
   it('renders login', () => {
-    render(<Login />);
+    <Provider store={store}>
+      render(<Login />, {});
+    </Provider>
   });
 });
