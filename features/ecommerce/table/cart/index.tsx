91e238a78346b4ccf29e8e36605da7932af2a499
U R U <uru@Uru-MacBook-Pro.local>
2023-06-13 10:35:36 +0545

    feat(table): added table component

--- features/ecommerce/table/cart/index.tsx
+++ features/ecommerce/table/cart/index.tsx
@@ -1,150 +1,79 @@
-import { Card, Col, List, Row, Space } from 'antd';
-import Image from 'next/image';
-// import { addtocart, heart, kfcimage, nonVeg } from 'imageConfig';
-import { useEffect, useState } from 'react';
+import { Button, Image, InputNumber, Space, Table } from 'antd';
+import { useState } from 'react';
 
+function CartComponent() {
+    const [cartItems, setCartItems] = useState([
+        {
+            key: '1',
+            image: 'https://images.kfcpakistan.com/image/1664273695795-image.png',
+            name: 'Product 1',
+            price: '$10.00',
+            quantity: 2,
+            subtotal: '$20.00',
+        },
+        {
+            key: '2',
+            image: 'https://images.kfcpakistan.com/image/1664273695795-image.png',
+            name: 'Product 2',
+            price: '$15.00',
+            quantity: 1,
+            subtotal: '$15.00',
+        },
+    ]);
 
-interface IProduct {
-    brand: string,
-    category: string,
-    description: string,
-    discountPercentage: number,
-    id: number,
-    images: Array<string>,
-    price: number,
-    rating: number,
-    stock: number,
-    thumbnail: string,
-    title: string
-}
+    const handleQuantityChange = (key: string, quantity: number) => {
+        const updatedCartItems = cartItems.map((item) =>
+            item.key === key ? { ...item, quantity } : item
+        );
+        setCartItems(updatedCartItems);
+    };
 
-const CartComponent = () => {
-    const [data, setData] = useState<Array<IProduct>>([]);
+    const handleRemoveItem = (key: string) => {
+        const updatedCartItems = cartItems.filter((item) => item.key !== key);
+        setCartItems(updatedCartItems);
+    };
 
+    const columns = [
+        {
+            title: 'Product Details',
+            dataIndex: 'product',
+            render: (text: any, record: any) => (
+                <Space size={'middle'}>
+                    <Image src={record.image} alt={record.name} width={80} />
+                    <span>{record.name}</span>
+                </Space>
+            ),
+        },
+        {
+            title: 'Price',
+            dataIndex: 'price',
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'quantity',
+            render: (text: any, record: any) => (
+                <InputNumber
+                    min={1}
+                    value={record.quantity}
+                    onChange={(value) => handleQuantityChange(record.key, value)}
+                />
+            ),
+        },
+        {
+            title: 'Subtotal',
+            dataIndex: 'subtotal',
+        },
+        {
+            title: 'Action',
+            render: (text: string, record: any) => (
+                <Button type="primary" onClick={() => handleRemoveItem(record.key)}>
+                    Remove
+                </Button>
+            ),
+        },
+    ];
 
-    const fetchProduct = async () => {
-        try {
-            var response = await fetch('https://dummyjson.com/products');
-            var jsonData = await response.json();
-            setData(jsonData.products);
-            // console.log('jsonData', jsonData.products);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    }
-    console.log("data", data);
-    useEffect(() => {
-        fetchProduct();
-    }, []);
-
-    return (
-        <>
-            <List
-                grid={{
-                    gutter: 16,
-                    xs: 1,
-                    sm: 2,
-                    md: 4,
-                    lg: 4,
-                    xl: 6,
-                    xxl: 3,
-                }}
-                dataSource={data}
-                renderItem={(item, index) => (
-                    <List.Item>
-                        <Card
-                            key={`product-${index}`}
-                            title={item?.title}
-                            cover={
-                                <Image
-                                    className='h-3'
-                                    src={item?.thumbnail}
-                                    alt={'product-image'}
-                                    height={100}
-                                    width={100}
-                                />
-                            }
-                        >Card content</Card>
-                    </List.Item >
-                )}
-            />
-            <>
-                <div>
-                    <Row gutter={16}>
-                        <Space direction="vertical" size={16}>
-                            {data !== null ? (
-                                <ul>
-                                    {data?.map((item: any, index) => (
-                                        <Card style={{ width: 300 }} key={`cards-${index}`}>
-                                            <div className="card-top" style={{ height: '282px' }}>
-                                                {/* <div style={{ position: 'relative' }}>
-                                            <Image src={kfcimage} alt="Example" />
-                                        </div>
-
-                                        <div style={{ position: 'absolute', left: 20, top: 20 }}>
-                                            <Image src={nonVeg} alt="" />
-                                        </div>
-                                        <div style={{ position: 'absolute', right: 20, top: 20 }}>
-                                            <Image src={heart} alt="" />
-                                        </div> */}
-                                                <div
-                                                    style={{
-                                                        position: 'absolute',
-                                                        top: '260px',
-                                                        color: '#FF8910',
-                                                        background: 'rgb(255, 137, 16, 0.2)',
-                                                        opacity: 0.6,
-                                                        borderRadius: '10px',
-                                                        padding: '4px 8px',
-                                                    }}
-                                                >
-                                                    25% off on deals
-                                                </div>
-                                            </div>
-
-                                            <div
-                                                style={{ borderTop: '1px solid black' }}
-                                            >
-                                                <h3>{item.title}</h3>
-                                                <p>{item.description}</p>
-                                                <div>
-                                                    <Row style={{ alignItems: 'center' }}>
-                                                        <Col span={12}>
-                                                            <span style={{ fontWeight: '700', color: '#32373D' }}>
-                                                                Rs {item.price}
-                                                            </span>
-                                                        </Col>
-                                                        <Col span={12}>
-                                                            <button
-                                                                style={{
-                                                                    float: 'right',
-                                                                    borderRadius: '12px',
-                                                                    background: '#E5002B',
-                                                                    color: '#fff',
-                                                                    border: 'none',
-                                                                    padding: '10px',
-                                                                }}
-                                                            >
-                                                                Add To Cart
-                                                                {/* <Image src={addtocart} />Add To Cart */}
-                                                            </button>
-                                                        </Col>
-                                                    </Row>
-                                                </div>
-                                            </div>
-                                        </Card>
-                                    ))}
-                                </ul>
-                            ) : (
-                                <p>Loading...</p>
-                            )}
-                        </Space>
-                    </Row>
-                </div>
-            </>
-        </>
-    );
+    return <Table columns={columns} dataSource={cartItems} pagination={false} />;
 }
 
 export default CartComponent;
-
