dca3d02f25a9adf3956adafec4b3e27df4548ae9
Sandeep Dangol <sandeep.dangol@ekbana.info>
2023-06-15 16:01:42 +0545

    feat(footer): footer design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { NextPageWithLayout } from "./_app";
 import MainLayout from "@/shared/main-layout";
+import Banner from "@/shared/components/banner";
 
 const Home: NextPageWithLayout = () => {
-  return <div className="text-9xl font-bold min-h-[300vh]">Home</div>;
+  return (
+    <div className="text-9xl font-bold min-h-[300vh]">
+      home
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
--- src/shared/lib/image-config.ts
+++ src/shared/lib/image-config.ts
@@ -1,2 +1,10 @@
 
 export const Logo = '/images/logo.svg';
+export const QR = '/images/qr__scan_2.png';
+export const AppStore = '/images/apple-store.jpg';
+export const PlayStore = '/images/play-store.jpg';
+export const socials = {
+    facebook: 'images/facebook.svg',
+    instagram: 'images/instagram.svg',
+    youtube: 'images/youtube.svg'
+}
