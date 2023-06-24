4ed68376caa4ac61685a6a3b682ba2aa063396f0
U R U <uru@Uru-MacBook-Pro.local>
2023-06-24 21:31:46 +0545

    basseurl: baseurl fix

--- src/services/offer.service.ts
+++ src/services/offer.service.ts
@@ -1,25 +1,24 @@
 import axiosInstance from "@/axios/axiosInstance";
-import { config } from "../../config";
-import axios from "axios";
 
-const baseURL = config.gateway.baseUrl;
-
-export const getOffers = async (query:any, page:any, offer:any, maxPrice:any, minPrice:any) => {
+export const getOffers = async (
+  query: any,
+  page: any,
+  offer: any,
+  maxPrice: any,
+  minPrice: any
+) => {
   try {
-    const response = await axiosInstance.get(`${baseURL}/product`, {
+    const response = await axiosInstance.get(`/product`, {
       params: {
         query,
         page,
         offer,
         maxPrice,
-        minPrice
-      }
+        minPrice,
+      },
     });
     return response.data;
   } catch (error) {
     throw error;
   }
 };
-
-
-
