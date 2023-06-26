72aaa8cf91fe833629c17d1724f7ebd500e3c1f9
Sandeep Risal <sandeep.risal@ekbana.info>
2023-06-26 17:03:41 +0545

    fix: fade effect added in banner

--- src/shared/components/banner/index.tsx
+++ src/shared/components/banner/index.tsx
@@ -7,6 +7,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import "swiper/css/effect-fade";
 import { useQuery } from "@tanstack/react-query";
 import { IHome } from "@/interface/home.interface";
 import { useRouter } from "next/router";
@@ -34,7 +35,8 @@ const Banner = () => {
               pagination={{
                 clickable: true,
               }}
-              modules={[Autoplay, Pagination]}
+              modules={[Autoplay, Pagination , EffectFade]}
+              effect="fade"
             >
               {data?.data?.adbanners.map((prev, index) => (
                 <SwiperSlide key={`banner-images-${index}`} onClick={() => router.push(`/${prev.websiteUrl}`)}>
