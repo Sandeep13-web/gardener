0ce3aac6137eb2d14ac7af95a8cde3aa671767a5
Sandeep Risal <sandeep.risal@ekbana.info>
2023-06-27 10:20:22 +0545

    fix: banner loader animation fix

--- src/shared/components/skeleton/banner/index.tsx
+++ src/shared/components/skeleton/banner/index.tsx
@@ -4,7 +4,7 @@ const BannerSkeletonLoader = () => {
     return (
         <div className="w-full rounded-md">
             <div className="flex space-x-4 animate-pulse">
-                <div className="w-full h-[300px] md:h-[calc(100vh-200px)] bg-gray-300"></div>
+                <div className="w-full h-[300px] md:h-[calc(100vh-200px)] bg-gray-400"></div>
             </div>
         </div>
     )
