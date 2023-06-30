2a8b83bb67e38f8bfe928671a1b90a12b1f0ca43
U R U <uru@Uru-MacBook-Pro.local>
2023-06-30 10:20:12 +0545

    add hooks

--- src/hooks/cart.hooks.tsx
+++ src/hooks/cart.hooks.tsx
@@ -1,19 +1,31 @@
-import { deleteCartItemById } from "@/services/cart.service";
+import { deleteCartItemById, updateCart } from "@/services/cart.service";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const useCarts = () => {
+    const [selectedId, setSelectedId] = useState<number>(0);
     const queryClient = useQueryClient();
+
     const cartDelete = useMutation({
         mutationFn: deleteCartItemById,
         onSuccess: () => {
             queryClient.invalidateQueries(['getCart'])
         }
     })
+
     const handleRemoveFromCart = (id: number) => {
+        setSelectedId(id)
         cartDelete.mutate(id)
     };
 
-    return { handleRemoveFromCart, cartDelete };
+    const updateCartMutation = useMutation({
+        mutationFn: updateCart,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['getCart'])
+        }
+    })
+
+
+    return { handleRemoveFromCart, cartDelete, selectedId, updateCartMutation };
 };
