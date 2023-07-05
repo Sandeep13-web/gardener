1435a0366c68a0e743b40aa6a6e5b18928cba19d
Saman Khadgi <saman1.khadgi@ekbana.info>
2023-07-05 10:47:20 +0545

    feat(blogs): blogs page design

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -2,12 +2,31 @@ import MainLayout from "@/shared/main-layout";
 import React from "react";
 import { NextPageWithLayout } from "../_app";
 import Breadcrumb from "@/components/Breadcrumb";
+import BlogsCard from "@/shared/components/blogsCard";
+import Pagination from "@/shared/components/pagination";
+import BlogSidebar from "@/shared/components/blogsSidebar";
 
 const Blogs: NextPageWithLayout = () => {
   return (
-  <div>
+    <div>
       <Breadcrumb />
-  </div>
+      <div className="container  my-[60px]">
+        <div className="grid grid-cols-12 md:gap-[30px]">
+          <div className="order-last md:order-first col-span-12 md:col-span-3 right-sidebar">
+            <BlogSidebar />
+          </div>
+          <div className="col-span-12 md:col-span-9">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+              <BlogsCard />
+              <BlogsCard />
+              <BlogsCard />
+              <BlogsCard />
+            </div>
+            <Pagination />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
