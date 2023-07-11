728e1c27b9e8fa8143ee5df5cbfd49ebffe19eff
Saman Khadgi <saman1.khadgi@ekbana.info>
2023-07-11 16:58:03 +0545

    feat(account): add address modal

--- src/pages/account/addresses/index.tsx
+++ src/pages/account/addresses/index.tsx
@@ -1,5 +1,6 @@
 import Breadcrumb from "@/components/Breadcrumb";
 import AccountSidebar from "@/shared/components/accountSidebar";
+import Button from "@/shared/components/button";
 import NewAddressIcon from "@/shared/icons/common/NewAddressIcon";
 import TrashIcon from "@/shared/icons/common/TrashIcon";
 import MainLayout from "@/shared/main-layout";
@@ -7,6 +8,9 @@ import React, { useState } from "react";
 import { FaPencilAlt, FaTrashAlt } from "react-icons/fa";
 
 const DelieveryAddress = () => {
+
+  
+  const [showModal, setShowModal] = useState<boolean>(false);
   return (
     <div className="bg-gray-100">
       <Breadcrumb />
@@ -21,10 +25,96 @@ const DelieveryAddress = () => {
             </h5>
             <div className="grid grid-cols-12 p-4 gap-5">
               <div className="col-span-12 sm:col-span-6 lg:col-span-4 border boder-solid boder-grey-500 min-h-[170px]">
-                <button className="w-full h-full flex gap-3 flex-col items-center justify-center text-center hover:bg-gray-200">
+                <button
+                  onClick={() => setShowModal(!showModal)}
+                  className="w-full h-full flex gap-3 flex-col items-center justify-center text-center hover:bg-gray-200"
+                >
                   <NewAddressIcon />
                   <span className="text-center">Add New Location</span>
                 </button>
+                {showModal && (
+                  <>
+                    <input
+                      type="checkbox"
+                      id="mymodal"
+                      className="modal-toggle"
+                      defaultChecked
+                    />
+                    <div className="modal">
+                      <div className="rounded-lg modal-box p-4">
+                        <div className="pb-2 border-b border-gray-300 ">
+                          <h3 className="text-lg font-medium">
+                            SET DELIEVERY LOCATION
+                          </h3>
+                          <p className="text-primary text-sm">
+                            {" "}
+                            Drag the map to pin point your delievery lcoation{" "}
+                          </p>
+                        </div>
+                        <form action="" className="py-4">
+                          <div className="h-[280px] mb-3"></div>
+                          <label
+                            htmlFor="addresstitle"
+                            className="block mb-2 text-sm"
+                          >
+                            {" "}
+                            Address Title *
+                          </label>
+                          <input
+                            type="text"
+                            id="addresstitle"
+                            className="input input-bordered w-full h-[42px] text-sm mb-4"
+                          />
+                          <label
+                            htmlFor="fullname"
+                            className="block mb-2 text-sm"
+                          >
+                            {" "}
+                            Full Name *
+                          </label>
+                          <input
+                            type="text"
+                            id="fullname"
+                            className="input input-bordered w-full h-[42px] text-sm mb-4"
+                          />
+                          <label
+                            htmlFor="number"
+                            className="block mb-2 text-sm"
+                          >
+                            {" "}
+                            Phone number *
+                          </label>
+                          <input
+                            type="number"
+                            id="number"
+                            className="input input-bordered w-full h-[42px] text-sm mb-4"
+                          />
+                          <div className="flex items-center gap-2 mb-4">
+                            <input type="checkbox" id="check" />
+                            <label htmlFor="check" className="text-sm">
+                              Set As Default
+                            </label>
+                          </div>
+                          <div className="mt-2 flex gap-4">
+                            <Button
+                              onClick={() => setShowModal(!showModal)}
+                              className="btn-error rounded-[30px] px-[30px] py-[11px]"
+                            >
+                              Cancel
+                            </Button>
+                            <Button className="btn rounded-[30px] px-[30px] py-[11px]">
+                              Save
+                            </Button>
+                          </div>
+                        </form>
+                      </div>
+                      <label
+                        className="modal-backdrop"
+                        htmlFor="mymodal"
+                      ></label>
+                    </div>
+                  </>
+                )}
               </div>
               <div className="col-span-12 sm:col-span-6 lg:col-span-4 border boder-solid boder-grey-500 min-h-[170px] p-4">
                 <h5 className="mb-2">Jwagal</h5>
@@ -36,7 +126,10 @@ const DelieveryAddress = () => {
                     <FaTrashAlt className="text-gray-400" />
                     Remove
                   </button>
-                  <button className="flex items-center gap-1">
+                  <button
+                    onClick={() => setShowModal(!showModal)}
+                    className="flex items-center gap-1"
+                  >
                     <FaPencilAlt className="text-gray-400" />
                     Edit
                   </button>
@@ -52,7 +145,10 @@ const DelieveryAddress = () => {
                     <FaTrashAlt className="text-gray-400" />
                     Remove
                   </button>
-                  <button className="flex items-center gap-1">
+                  <button
+                    onClick={() => setShowModal(!showModal)}
+                    className="flex items-center gap-1"
+                  >
                     <FaPencilAlt className="text-gray-400" />
                     Edit
                   </button>
@@ -68,6 +164,6 @@ const DelieveryAddress = () => {
 
 export default DelieveryAddress;
 
-DelieveryAddress.getLayout = (page:any) => {
+DelieveryAddress.getLayout = (page: any) => {
   return <MainLayout>{page}</MainLayout>;
 };
