ac4644c3670817b816ee6764afa5ccbb4bffc96b
Sandeep Risal <sandeep.risal@ekbana.info>
2023-07-18 15:02:47 +0545

    fix: account sidebar fixes

--- src/shared/components/accountSidebar/index.tsx
+++ src/shared/components/accountSidebar/index.tsx
@@ -6,14 +6,12 @@ const AccountSidebar = () => {
   const { pathname } = useRouter()
   const listItemClass = `group text-gray-400 relative p-4 flex gap-2 items-center border-none md:border-b border-gray-350 border-solid text-[14px]`;
   const linkClass = "absolute top-0 left-0 w-full h-full";
-  const iconClass = "group-hover:text-primary-focus w-5 h-auto md:w-full";
+  const iconClass = "group-hover:text-primary-focus w-5 h-auto md:w-auto";
   const linkUrls = [
     { href: `/account/profile`, icon: <FaUserAlt className={iconClass} />, text: "ACCOUNT DETAILS" },
     { href: `/account/order`, icon: <FaShoppingBag className={iconClass} />, text: "ORDER HISTORY" },
     { href: `/account/change-password`, icon: <FaShoppingBag className={iconClass} />, text: "CHANGE PASSWORD" },
     { href: `/account/addresses`, icon: <FaShoppingBag className={iconClass} />, text: "MY ADDRESS" },
-    { href: `/account/change-password`, icon: <FaSignOutAlt className={iconClass} />, text: "LOGOUT" },
-    { href: `/account/change-password`, icon: <FaTrashAlt className={iconClass} />, text: "DELETE ACCOUNT" },
   ];
 
   return (
@@ -36,6 +34,18 @@ const AccountSidebar = () => {
             <span className={`${iconClass} hidden md:block`}>{link.text}</span>
           </li>
         ))}
+        <li>
+          <button className={listItemClass}>
+            <FaSignOutAlt className={iconClass} />
+            <span className={`${iconClass} hidden md:block`}>Logout</span>
+          </button>
+        </li>
+        <li>
+          <button className={listItemClass}>
+            <FaTrashAlt className={iconClass} />
+            <span className={`${iconClass} hidden md:block`}>Delete Account</span>
+          </button>
+        </li>
       </ul>
     </div>
   );
