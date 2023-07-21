5605588d6fdcf28465b4375b2d3647021cdccf15
U R U <uru@Uru-MacBook-Pro.local>
2023-07-21 20:05:54 +0545

    fix: css import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import "@/styles/globals.scss";
 import 'react-toastify/dist/ReactToastify.css';
+
+import "@/styles/abstract/utils.scss";
 import "@/styles/pages/innerpages.scss";
 import "@/styles/pages/account.scss";
+import "@/styles/components/header.scss";
+import "@/styles/components/card.scss";
+import "@/styles/components/pageBanner.scss";
+import "@/styles/components/pagination.scss";
+import "@/styles/components/auth.scss";
+import "@/styles/components/btn.scss";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NextPage } from "next";
